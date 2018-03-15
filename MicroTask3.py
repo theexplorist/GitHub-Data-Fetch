4cd137207d87ed20c330fed0d1bb786fcfb5977b
Kartik Sapra <kartiksapra005@gmail.com>
2018-03-15 17:34:05 +0530

    complete

---
+++ MicroTask3.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import csv
+
+user = "aimacode" #username whose github is to be scrapped
+url = "https://api.github.com/repos/" + user + "/aima-python/contributors"
+response = requests.get(url)
+data = response.text
+
+parsed = json.loads(data)
+dat = []
+list1 = []
+for data1 in parsed :
+    dat.append(data1['login'])
+    list1.append(data1['contributions'])
+    print((data1['login'] + ":").ljust(30) +str(data1['contributions']))
+
+zip(dat, list1)
+
+with open('micro.csv', 'w', encoding = "ISO-8859-1", newline='') as f:
+    writer = csv.writer(f, delimiter='\t')
+    fieldnames = ['Name', 'Commits']
+    writer.writerow(fieldnames)
+    writer.writerows(zip(dat, list1))
